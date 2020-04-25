7cc637c087501e95bfa2d625d5f41dfb535145f2
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-04-25 12:10:11 -0300

    index.js has been changed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./containers/Pet";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-import {petApp} from "./reducers/index";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 
-//Crear el store , pasandole como parámetro el reducer
-const store = createStore(petApp);
-
-ReactDOM.render(
-            <Provider store={store}>
-                <App />
-            </Provider>
-            , document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
