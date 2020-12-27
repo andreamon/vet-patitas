f460cd1267310324fd4d82d88ab19bdf2f6a6881
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-12-27 19:48:52 -0300

    button cart added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import AppContext from "../context/AppContext";
 
 const Header = () => {
+  const {
+    state: { cart },
+  } = useContext(AppContext);
+
   return (
-    <Navbar bg="light" expand="lg" variant="light">
-      <Link to="/" style={{color:'#1697E9', textDecoration:'none', padding:'8px'}}>
-        <h4><strong>Patitas Suaves</strong></h4>
+    <Navbar bg="transparent" expand="lg" variant="light">
+      <Link
+        to="/"
+        style={{ color: "#1697E9", textDecoration: "none", padding: "8px" }}
+      >
+        <h4>
+          <strong>Patitas Suaves Pet Store</strong>
+        </h4>
       </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Link to="/" style={{color:'#1697E9', textDecoration:'none', padding:'8px'}}>Inicio</Link>
-          <Link to="/addPet" style={{color:'#1697E9', textDecoration:'none', padding:'8px'}}>Agregar</Link>
-          <Link to="/listAdopted" style={{color:'#1697E9', textDecoration:'none', padding:'8px'}}>Adoptados</Link>
+          <Link
+            to="/"
+            style={{ color: "#1697E9", textDecoration: "none", padding: "8px" }}
+          >
+            Inicio
+          </Link>
+          <Link
+            to="/addPet"
+            style={{ color: "#1697E9", textDecoration: "none", padding: "8px" }}
+          >
+            Agregar
+          </Link>
+          <Link
+            to="/listAdopted"
+            style={{ color: "#1697E9", textDecoration: "none", padding: "8px" }}
+          >
+            Adoptados
+          </Link>
+          <Button variant="link">
+            <i class="fas fa-shopping-cart" /> 
+            {cart.length > 0 && <span className="cart-header">{cart.length}</span>}
+          </Button>
         </Nav>
 
         {/* <Form inline>
